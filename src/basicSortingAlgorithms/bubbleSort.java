844ebc63255c6be50117f08efe5b54222ed32145
viren <virendra.sharma@evaluationz.com>
2019-07-18 14:23:38 +0530

    VS - Bubble sort

--- src/basicSortingAlgorithms/bubbleSort.java
+++ src/basicSortingAlgorithms/bubbleSort.java
@@ -2,8 +2,31 @@ package basicSortingAlgorithms;
 
 public class bubbleSort {
 
+    private void bubbleSortImplementation(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j+1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        printArray(array);
+    }
+
+    private void printArray(int[] array) {
+        for (int anArray : array) {
+            System.out.print(anArray + " ");
+        }
+        System.out.println();
+    }
+
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int array[] = {64, 34, 90, 25, 12, 22, 11};
+        bubbleSort result = new bubbleSort();
+        result.bubbleSortImplementation(array);
     }
 }
